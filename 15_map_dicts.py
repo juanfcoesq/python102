930020f589cb06e30b3f081a0153addf7aa60363
juanfcoesquivel0@gmail.com <123575915+juanfcoesq@users.noreply.github.com>
2025-07-02 08:45:21 -0600

    clase 15

---
+++ 15_map_dicts.py
@@ -0,0 +1,26 @@
+﻿items = [
+    {
+        'product' : 'camisa',
+        'price' : 100
+    },
+    {
+        'product' : 'pantalón',
+        'price' : 200
+    },
+    {
+        'product' : 'chuleta',
+        'price' : 300
+    }
+]
+
+prices = list(map(lambda item: item['price'], items))
+print(items)
+print(prices)
+
+def add_taxes(item):
+    item['taxes'] = item['price'] * 0.19
+    return item
+
+new_items = list(map(add_taxes, items))
+print(items)
+print(new_items)
